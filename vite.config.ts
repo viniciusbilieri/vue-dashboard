430f43b56ec7d80e75fafbcefc799909e278a3cd
dssViniciusBilieri <vinicius.bilieri@v.consultdss.com>
2025-10-06 19:57:25 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Dashboard-vue/', // <= igual ao nome do repo!
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
