6474e7deaa1342483335d96a8842a2f3cfef7944
Vinicius Bilieri <vinicius.bilieri@gmail.com>
2025-10-06 20:01:06 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/vue-dashboard/', // <= igual ao nome do repo!
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
